e74bcb476d6914f771afc9d914619b3895320d39
Ekaterina Goponenko <>
2020-10-01 22:45:49 +0300

    Make NavBar responsive

--- src/Header/Header.style.tsx
+++ src/Header/Header.style.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components/macro'
+import {ReactComponent as menu} from '../assets/images/menu.svg';
 
 export const HeaderWrapper = styled.div`
   background-color: var(--Main600);
@@ -6,4 +7,14 @@ export const HeaderWrapper = styled.div`
   align-items: center;
   width: 100%;
   height: var(--HeaderHeight);
+`
+// TODO Create svg with fill attibute to change color easily
+export const Menu = styled(menu)`
+  margin-left: 3vh;
+  color: var(--Main100);
+  display: none;
+  
+  @media (max-width: 768px) {
+    display: block;
+  }
 `
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { HeaderWrapper } from "./Header.style";
+import React, {useState} from 'react';
+import {HeaderWrapper, Menu} from './Header.style';
 
-class Header extends React.Component {
-    render() {
-        return <HeaderWrapper/>;
-    }
+const Header = () => {
+  const [isMenu, setMenu] = useState(false);
+
+  const openMenu = () => {
+    // TODO: Add dropdown menu instead of navigation menu
+    setMenu(!isMenu);
+  }
+
+  return (<HeaderWrapper>
+      <Menu onClick={openMenu}/>
+    </HeaderWrapper>
+  )
 }
 
 export default Header;
